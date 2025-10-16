6fd47bd698a86a03ad2d65b933c573ac4b691c44
EmrhnEmiroglu <emirhanemiroglu33@gmail.com>
2025-10-16 16:28:42 +0300

    4. Hafta Projesi

---
+++ GP_Hafta4.3(Proje)/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace GP_Hafta4._3_Proje_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnbaslat = new System.Windows.Forms.Button();
+            this.btnbitir = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnbaslat
+            // 
+            this.btnbaslat.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnbaslat.Location = new System.Drawing.Point(12, 167);
+            this.btnbaslat.Name = "btnbaslat";
+            this.btnbaslat.Size = new System.Drawing.Size(105, 80);
+            this.btnbaslat.TabIndex = 0;
+            this.btnbaslat.Text = "Oyuna Başla";
+            this.btnbaslat.UseVisualStyleBackColor = false;
+            this.btnbaslat.Click += new System.EventHandler(this.btnbaslat_Click);
+            // 
+            // btnbitir
+            // 
+            this.btnbitir.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnbitir.Location = new System.Drawing.Point(653, 64);
+            this.btnbitir.Name = "btnbitir";
+            this.btnbitir.Size = new System.Drawing.Size(125, 88);
+            this.btnbitir.TabIndex = 0;
+            this.btnbitir.Text = "Oyunu Bitir";
+            this.btnbitir.UseVisualStyleBackColor = false;
+            this.btnbitir.Click += new System.EventHandler(this.btnbitir_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.groupBox1.Location = new System.Drawing.Point(123, 16);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 410);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oyun Alanı";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(471, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(153, 372);
+            this.listBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnbitir);
+            this.Controls.Add(this.btnbaslat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbaslat;
+        private System.Windows.Forms.Button btnbitir;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GP_Hafta4.3(Proje)/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GP_Hafta4._3_Proje_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sure = 60;
+        Random rnd = new Random();
+        Label lbl = new Label();
+        List<Button> oyunButonlari = new List<Button>();
+        List<int> secilenSayilar = new List<int>();
+        List<int> dogruSira = new List<int>();
+        Timer timerHareket = new Timer();
+        bool oyunDevam = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Controls.Add(lbl);
+            lbl.Location = new Point(517, 150);
+            lbl.Width = 100;
+            lbl.Height = 100;
+            lbl.Text = "SÜRE\n60 sn";
+
+            
+            timerHareket.Interval = 1000;
+            timerHareket.Tick += TimerHareket_Tick;
+        }
+
+        private void btnbaslat_Click(object sender, EventArgs e)
+        {
+            
+            foreach (var btn in oyunButonlari)
+            {
+                groupBox1.Controls.Remove(btn);
+            }
+            oyunButonlari.Clear();
+            secilenSayilar.Clear();
+            listBox1.Items.Clear();
+            dogruSira.Clear();
+
+            
+            sure = 60;
+            lbl.Text = "SÜRE\n60 sn";
+            oyunDevam = true;
+
+            
+            List<int> sayilar = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                sayilar.Add(rnd.Next(1, 101));
+            }
+
+            
+            dogruSira = sayilar.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Button btn = new Button();
+                btn.BackColor = Color.White;
+                groupBox1.Controls.Add(btn);
+                btn.Width = 40;
+                btn.Height = 40;
+                btn.Name = "btn" + (i + 1);
+                btn.Text = sayilar[i].ToString();
+                btn.Tag = sayilar[i];
+                btn.Click += OyunButonu_Click;
+
+                oyunButonlari.Add(btn);
+            }
+
+            
+            ButonlariYerlestir();
+
+            
+            timer1.Start();
+            timerHareket.Start();
+        }
+
+        private void ButonlariYerlestir()
+        {
+            List<Rectangle> kullanilanAlanlar = new List<Rectangle>();
+
+            foreach (var btn in oyunButonlari)
+            {
+                bool uygunYerBulundu = false;
+                int deneme = 0;
+
+                while (!uygunYerBulundu && deneme < 100)
+                {
+                    int x = rnd.Next(5, groupBox1.Width - btn.Width - 5);
+                    int y = rnd.Next(20, groupBox1.Height - btn.Height - 5);
+
+                    Rectangle yeniAlan = new Rectangle(x, y, btn.Width, btn.Height);
+
+                    bool cakisiyor = false;
+                    foreach (var alan in kullanilanAlanlar)
+                    {
+                        Rectangle genisletilmisAlan = new Rectangle(
+                            alan.X - 5,
+                            alan.Y - 5,
+                            alan.Width + 10,
+                            alan.Height + 10
+                        );
+
+                        if (genisletilmisAlan.IntersectsWith(yeniAlan))
+                        {
+                            cakisiyor = true;
+                            break;
+                        }
+                    }
+
+                    if (!cakisiyor)
+                    {
+                        btn.Location = new Point(x, y);
+                        kullanilanAlanlar.Add(yeniAlan);
+                        uygunYerBulundu = true;
+                    }
+
+                    deneme++;
+                }
+
+                if (!uygunYerBulundu)
+                {
+                    int x = rnd.Next(5, groupBox1.Width - btn.Width - 5);
+                    int y = rnd.Next(20, groupBox1.Height - btn.Height - 5);
+                    btn.Location = new Point(x, y);
+                }
+            }
+        }
+
+        private void TimerHareket_Tick(object sender, EventArgs e)
+        {
+            if (oyunDevam)
+            {
+                ButonlariYerlestir();
+            }
+        }
+
+        private void OyunButonu_Click(object sender, EventArgs e)
+        {
+            if (!oyunDevam) return;
+
+            Button btn = (Button)sender;
+            int sayi = (int)btn.Tag;
+
+            secilenSayilar.Add(sayi);
+            listBox1.Items.Add(sayi);
+
+            btn.Enabled = false;
+            btn.BackColor = Color.LightGray;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            sure--;
+
+            if (sure == 0)
+            {
+                OyunBitti(false, "Süreniz doldu!\nBaşarısız oldunuz. Tekrar deneyin.");
+                lbl.Text = "SÜRE\n0 sn";
+            }
+            else
+            {
+                lbl.Text = "SÜRE\n" + sure + " sn";
+            }
+        }
+
+
+        private void OyunBitti(bool kazandi, string mesaj)
+        {
+            timer1.Stop();
+            timerHareket.Stop();
+            oyunDevam = false;
+
+            foreach (var btn in oyunButonlari)
+            {
+                btn.Enabled = false;
+            }
+
+            MessageBox.Show(mesaj, kazandi ? "Başarılı!" : "Başarısız!",
+                MessageBoxButtons.OK,
+                kazandi ? MessageBoxIcon.Information : MessageBoxIcon.Warning);
+        }
+
+        private void btnbitir_Click_1(object sender, EventArgs e)
+        {
+            if (!oyunDevam)
+            {
+                MessageBox.Show("Önce oyunu başlatmalısınız!", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var secilenCiftler = secilenSayilar.Where(x => x % 2 == 0).ToList();
+
+            if (secilenCiftler.Count == 0)
+            {
+                OyunBitti(false, "Hiç çift sayı seçmediniz!\nBaşarısız oldunuz. Tekrar deneyin.");
+                return;
+            }
+
+            var secilenTekler = secilenSayilar.Where(x => x % 2 != 0).ToList();
+            if (secilenTekler.Count > 0)
+            {
+                OyunBitti(false, "Tek sayı seçtiniz! Sadece çift sayıları seçmelisiniz.\nBaşarısız oldunuz. Tekrar deneyin.");
+                return;
+            }
+
+            if (secilenCiftler.Count != dogruSira.Count)
+            {
+                OyunBitti(false, $"Tüm çift sayıları seçmediniz!\nSeçilen: {secilenCiftler.Count}, Olması gereken: {dogruSira.Count}\nBaşarısız oldunuz. Tekrar deneyin.");
+                return;
+            }
+
+            bool dogruMu = true;
+            for (int i = 0; i < dogruSira.Count; i++)
+            {
+                if (secilenCiftler[i] != dogruSira[i])
+                {
+                    dogruMu = false;
+                    break;
+                }
+            }
+
+            if (dogruMu)
+            {
+                OyunBitti(true, $"Tebrikler!\n{secilenCiftler.Count} sayıyı doğru sırayla seçtiniz.\n{60 - sure} saniyede tamamladınız.\nOyunu kazandınız!");
+            }
+            else
+            {
+                OyunBitti(false, "Yanlış sıralama! Çift sayıları küçükten büyüğe seçmelisiniz.\nBaşarısız oldunuz. Tekrar deneyin.");
+            }
+        }
+    }
+}
